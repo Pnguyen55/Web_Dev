5672627e24fd3e45473530a906ecdac0f31a3e60
Pnguyen55 <p.nguyen_55@yahoo.com>
2018-07-18 12:02:13 -0400

    Finished Javascripts Array section

--- javascript/ToDoList.js
+++ javascript/ToDoList.js
@@ -5,13 +5,15 @@ window.setTimeout(function() {
 	var input = prompt("What would you like to do?");
 
 	while(input != "quit") {
+		//handle input
 		if(input == "list") {
-			console.log(todos)
+			listTodos();
 		}
 		else if(input == "new"){
-
-			var newTodo = prompt("Enter new todo");
-			todos.push(newTodo);
+			addTodos();
+		}
+		else if(input == "delete"){
+			deleteTodos();
 		}
 
 		input = prompt("What would you like to do?")
@@ -20,3 +22,29 @@ window.setTimeout(function() {
 }, 500);
 
 console.log("Okay, you quit the app")
+
+function listTodos(){
+	for(var i = 0; i < todos.length;i++){
+		console.log("***********");
+		console.log(i + ": " +todos[i]);
+	}
+}
+
+function addTodos(){
+	//ask for new todo, and pushes it to the end of the array
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+
+	//displaying added element
+	console.log("Added: " + newTodo);
+}
+
+function deleteTodos(){
+	//delete todo from array
+
+	var indexDelete = prompt("Enter index of todo to delete");
+			
+	var deleted = todos.splice(indexDelete,1);
+	//displaying deleted element
+	console.log("Deleted: " + deleted);
+}
---
+++ javascript/practice_array_script.js
@@ -0,0 +1,41 @@
+
+function printReverse(array){
+	for(i = array.length-1; i >= 0; i--){
+		console.log(array[i]);
+	}
+}
+
+function isUniform(array){
+
+	var theTruth = array[0];
+
+	for(var i = 0; i < array.length; i++){
+		if(array[i] != theTruth){
+			return false;
+		}
+	}
+
+	return true;
+}
+
+function sumArray(array){
+	var sum = 0;
+
+	for(var i = 0; i < array.length; i++){
+		sum += array[i];
+	}
+
+	return sum;
+}
+
+function max(array){
+	var max = array[0];
+
+	for(var i = 0; i < array.length; i++){
+		if(array[i] > max){
+			max = array[i];
+		}
+	}
+
+	return max;
+}
